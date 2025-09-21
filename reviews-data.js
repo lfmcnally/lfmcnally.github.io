9d65d3cba292ecb1b22d8c957d5906873d138125
lfmcnally <lawrencefmcnally@gmail.com>
2025-09-21 18:18:41 +0100

    Update reviews-data.js

--- reviews-data.js
+++ reviews-data.js
@@ -7,6 +7,11 @@ const reviewsData = [
         role: "Freelance Tutor - Latin/Classical Civilisation/English",
         text: "Classicalia has been a fantastic resource to help me navigate teaching Latin GCSE for the first time as a freelance tutor. The website provides an abundance of helpful resources and interactive texts that are incredibly user friendly and laid out in a clear, coherent and engaging way. Lawrence himself has been extremely helpful and generous in his guidance!"
     },
+    {
+        name: "The Classical Association",
+        role: "Professional Organisation for Classics Education",
+        text: "Lawrence radiates the generous spirit of the Classics teaching community and deserves to be recognised for his significant achievements. He has made syllabus-linked student workbooks for A Level Classical Civilisation and GCSE Latin which he hosts, for free, on his personal website. These are high quality resources which make a real difference to students and teachers alike."
+    }
     // Add more reviews here following the same format:
     // {
     //     name: "Your Name",
