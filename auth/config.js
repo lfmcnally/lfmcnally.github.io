1ec3dc91d35553222b23ae4d1f24def21c5e8d7b
lfmcnally <lawrencefmcnally@gmail.com>
2025-11-26 20:27:31 +0000

    Update config.js

--- auth/config.js
+++ auth/config.js
@@ -7,7 +7,3 @@
 
 const SUPABASE_URL = 'https://kqdnqaqqhpdryzoqvdac.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxZG5xYXFxaHBkcnl6b3F2ZGFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzYwNjYsImV4cCI6MjA3OTc1MjA2Nn0.TLrLDe8NpzNtyHrBjVKsTPAG4drErUW3dZ9zfrLhOeE';
-
-// Create the Supabase client
-// This is what we use to talk to the database
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
